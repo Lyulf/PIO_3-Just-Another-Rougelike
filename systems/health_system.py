04718645a7a0f82d6a807642311fb421bee0fb8e
Karol Kwapinski <karol.kwapinski1@interia.pl>
2023-06-17 21:16:47 +0200

    KCK-22 Dodanie podnoszenia przedmiotow przez gracza

--- systems/health_system.py
+++ systems/health_system.py
@@ -1,11 +1,27 @@
 import random
 from systems.system import *
 
+class ItemType(Enum):
+    ARMOR = 0
+    PISTOL = 1
+    SHOTGUN = 2
+    RIFLE = 3
+
 class HealthSystem(System):
     def on_fixed_update(self):
         for entity in self.entity_manager.get_entities():
             components = self.component_manager.get_components(entity, HealthComponent, ImageSpriteComponent,
                                                                TransformComponent)
+            item_type = random.choice(list(ItemType))
+            name = 'armor'
+            if item_type == ItemType.ARMOR:
+                name = 'armor'
+            elif item_type == ItemType.SHOTGUN:
+                name = 'shotgun'
+            elif item_type == ItemType.PISTOL:
+                name = 'pistol'
+            elif item_type == ItemType.RIFLE:
+                name = 'rifle'
             try:
                 health = components[HealthComponent]
                 image_sprite = components[ImageSpriteComponent]
@@ -18,7 +34,7 @@ class HealthSystem(System):
                     image_sprite.current_sprite = image_sprite.sprite_sheets['death']
                     if image_sprite.current_sprite.is_finished:
                         if health.entity_type == EntityType.ENEMY and random.random() < 0.5:
-                            self.prefab_manager.spawn('item', transform.position)
+                            self.prefab_manager.spawn(name, transform.position)
                         entity.is_alive = False
                 except KeyError:
                     entity.is_alive = False
