c4165f0d2979003abb81e1ca81c6eecae0a97436
Lyulf <patryk.chodorowski0@gmail.com>
2023-06-18 08:09:53 +0200

    KCK-52 Fix bullets phasing through enemies

--- systems/damage_system.py
+++ systems/damage_system.py
@@ -1,3 +1,5 @@
+import pygame
+
 from systems.system import *
 from entities.entity import Entity
 
@@ -6,7 +8,8 @@ class DamageSystem(System):
         entities = self.entity_manager.get_entities()
         ticks = pygame.time.get_ticks()
         for idx, lhs_entity in enumerate(entities):
-            lhs_components = self.component_manager.get_components(lhs_entity, TransformComponent, RectHitboxComponent, HealthComponent, DamageComponent)
+            lhs_components = self.component_manager.get_components(
+                lhs_entity, TransformComponent, RigidbodyComponent, RectHitboxComponent, HealthComponent, DamageComponent)
             try:
                 lhs_transform = lhs_components[TransformComponent]
                 lhs_rect_hitbox = lhs_components[RectHitboxComponent]
@@ -14,15 +17,36 @@ class DamageSystem(System):
                 continue
 
             for rhs_entity in entities[idx + 1:]:
-                rhs_components = self.component_manager.get_components(rhs_entity, TransformComponent, RectHitboxComponent, HealthComponent, DamageComponent)
+                rhs_components = self.component_manager.get_components(
+                    rhs_entity, TransformComponent, RigidbodyComponent, RectHitboxComponent, HealthComponent, DamageComponent)
                 try:
                     rhs_transform = rhs_components[TransformComponent]
                     rhs_rect_hitbox = rhs_components[RectHitboxComponent]
                 except (TypeError, KeyError):
                     continue
 
-                lhs_rect = lhs_rect_hitbox.rect.move(lhs_transform.position - lhs_rect_hitbox.anchor)
-                rhs_rect = rhs_rect_hitbox.rect.move(rhs_transform.position - rhs_rect_hitbox.anchor)
+                lhs_position = lhs_transform.position - lhs_rect_hitbox.anchor
+                rhs_position = rhs_transform.position - rhs_rect_hitbox.anchor
+
+                try:
+                    lhs_rigidbody = lhs_components[RigidbodyComponent]
+                except AttributeError:
+                    lhs_velocity = pygame.Vector2()
+                else:
+                    lhs_velocity = lhs_rigidbody.speed * lhs_rigidbody.direction
+
+                try:
+                    rhs_rigidbody = rhs_components[RigidbodyComponent]
+                except AttributeError:
+                    rhs_velocity = pygame.Vector2()
+                else:
+                    rhs_velocity = rhs_rigidbody.speed * rhs_rigidbody.direction
+
+                lhs_position, rhs_position = self.__find_positions_at_minimum_distance(
+                    lhs_position, lhs_velocity, rhs_position, rhs_velocity)
+
+                lhs_rect = lhs_rect_hitbox.rect.move(lhs_position)
+                rhs_rect = rhs_rect_hitbox.rect.move(rhs_position)
                 if not lhs_rect.colliderect(rhs_rect):
                     continue
 
@@ -42,6 +66,28 @@ class DamageSystem(System):
                 else:
                     self.__do_damage(lhs_entity, rhs_health, lhs_damage, ticks)
 
+    def __find_positions_at_minimum_distance(self, lhs_position, lhs_velocity, rhs_position, rhs_velocity):
+        ms_since_last_fixed_update = 50
+        lhs_last_position = lhs_position - ms_since_last_fixed_update * lhs_velocity
+        rhs_last_position = rhs_position - ms_since_last_fixed_update * rhs_velocity
+
+        p = lhs_last_position - rhs_last_position
+        v = lhs_velocity - rhs_velocity
+
+        # time when vectors are perpendicular (minimum distance between objects)
+        try:
+            t = -p.dot(v) / v.magnitude_squared()
+        except:
+            return lhs_position, rhs_position
+
+        t = pygame.math.clamp(t, 0, ms_since_last_fixed_update)
+
+        print(t)
+
+        lhs_position = lhs_last_position + t * lhs_velocity
+        rhs_position = rhs_last_position + t * rhs_velocity
+
+        return lhs_position, rhs_position
 
     def __do_damage(self, damage_source_entity: Entity, health_component: HealthComponent, damage_component: DamageComponent, current_ticks: int):
         if health_component.entity_type in damage_component.ignore_entity_types:
